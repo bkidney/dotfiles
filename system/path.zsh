49fbfce97098f8901171737c862a5e84880c2e58
Brian Kidney <bkidney@ieee.org>
2013-12-20 13:48:45 -0330

    Removes OSX dependent directory references.

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,5 @@
 export PATH="/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-export PATH=/Users/bkidney/bin:"$PATH"
-export PATH=/Users/bkidney/Dropbox/bin:"$PATH"
+export PATH="$HOME/bin:$PATH"
+export PATH="$HOME/Dropbox/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
