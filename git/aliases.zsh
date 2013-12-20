5990d40dfd1cc804dcdf614c8f8367fcd63664b0
Brian Kidney <bkidney@ieee.org>
2013-12-20 00:01:11 -0330

    Updates grm and adds gitpom aliases.
    Updates grm to work with filename containing spaces.
    Adds gitpom - git push origin master

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,3 +1,8 @@
 # The rest of my fun git aliases
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+grm() {
+  git status | grep deleted | sed -e "s/[[:blank:]]*deleted:[[:blank:]]*//g" -e "s/\(^.*$\)/\"\1\" /g" |xargs git rm
+}
+
+alias gitpom="git push origin master"
