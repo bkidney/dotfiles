a32f635bec9b7cdbb8800b7c87971fe151ae52c0
Brian Kidney <bkidney@ieee.org>
2014-03-11 10:41:57 -0230

    Moves "gitpom" alias to git alias "git pom"

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,5 +4,3 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 grm() {
   git status | grep deleted | sed -e "s/[[:blank:]]*deleted:[[:blank:]]*//g" -e "s/\(^.*$\)/\"\1\" /g" |xargs git rm
 }
-
-alias gitpom="git push origin master"
