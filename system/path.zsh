3380e237568bff44a971dbc252f05e52ae2a634c
Brian Kidney <bkidney@briankidney.ca>
2018-02-14 11:32:13 -0330

    Fixes path issus for .dotfiles/bin

--- system/path.zsh
+++ system/path.zsh
@@ -2,8 +2,7 @@ if [[ $IS_MAC ]]; then
     export PATH="$PATH:/usr/local/bin"
 fi
 
-export PATH="$PATH;$ZSH/bin"
+export PATH="$ZSH/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
-export PATH="$HOME/Dropbox/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
