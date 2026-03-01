c0607af78e47884af11fe6b4d91972f19a2a3d65
Brian Kidney <bkidney@briankidney.ca>
2026-03-01 13:47:11 -0330

    Adds some network alias for FreeBSD laptop.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,9 @@ alias reload!='. ~/.zshrc'
 if [[ $IS_MAC ]]; then
 	alias grammarly="open -a Grammarly"
 fi
+
+if [[ $IS_LAPTOP ]]; then
+	alias ethup="sudo service dhcpd restart ue0"
+	alias wifidown="sudo serivce netif stop wlan0"
+	alias wifiup="sudo serivce netif start wlan0"
+fi
